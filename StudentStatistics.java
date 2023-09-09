d8f0b50445a79e54e27307a334dfa7971ad690da
PradeepDixit097 <140936068+PradeepDixit097@users.noreply.github.com>
2023-09-09 18:17:46 +1000

    First Requirements Assignment
    allows the user to input the assignment name

---
+++ StudentStatistics.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * Program to compute statistics of 'students' marks in an assignment.
+ * @author (Pradeep Mani Dixit)
+ * @version (6/09/2023)
+ */
+public class StudentStatistics
+{
+    public static void main(String[] args) {
+        // Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // let the user to enter the assignment name
+        System.out.print("Enter the Name of the Assignment: ");
+
+        // Read input from the user and store in a variable
+        String assignmentName = scanner.nextLine();
+        
+       
+        
+    }
+}
